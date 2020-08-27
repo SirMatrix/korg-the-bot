3dbfa15d65de84fb8574cb8e53c58db2f713af12
Sir Matrix <killer20816@gmail.com>
2020-08-27 21:50:26 +0000

    Updated Cargo and bot calliing. Serenitys Documentation is pretty ehh

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,18 @@
-
-use serenity::client::Context;
 use serenity::prelude::*;
 use serenity::model::prelude::*;
-use serenity::model::*;
-use tokio_core::reactor::Handle;
-use std::collections::HashMap;
-use serenity::framework::Framework;
-
-
-
-
-
-
-const MOD_ROLE: RoleId = RoleId(692794426413547530);
-const GUILD_ID: GuildId = GuildId(692401689323503637);
-
-
-
-struct MyFramework{
-    commands: HashMap<String, Box<Fn(Message, Vec<String>)>>,
-}
-
-
-impl Framework for MyFramework {
-    fn dispatch(&mut self, _: Context, msg: Message, tokio_handle: &Handle){
-        let args = msg.content.split_whitespace();
-        let command = match args.advance() {
-            Some(command) => {
-                if !command.starts_with('~') {return;}
-            },
-            None => return,
-        };
-        let command = match self.commands.get(&command){
-            Some(command) => command, None => return,
-        };
-        tokio_handle.spawn_fn(move || {(command)(msg,args); Ok() });
-    }
-
-}
+use serenity::Client;
 
 struct Handler;
 
-
+#[serenity::async_trait]
 impl EventHandler for Handler {
+    async fn message(&self, context: Context, msg: Message) {
+        if msg.content == "!ping" {
+            let _ = msg.channel_id.say(&context, "Pong!");
+        }
+    }
 }
 
+let mut client = Client::new("my token here").event_handler(Handler).await?;
 
-
-use serenity::Client;
-
-
-fn main(){
-
-
-    let mut client = Client::new(&include_str!("bot_token.txt"),Handler).expect("Could not create new client.");
-
-    client.start().expect("Could not start client.");
-
-}
+client.start().await?;
