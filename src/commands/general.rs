707a4727a91012ed75f88f4222d36d3468f403cd
Sir Matrix <killer20816@gmail.com>
2020-08-28 21:48:34 +0000

    Commands directoy. This stores all our command files.

---
+++ src/commands/general.rs
@@ -0,0 +1,29 @@
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+framework::standard::macros::{command, group},
+framework::standard::{StandardFramework, CommandResult},
+};
+
+#[group]
+#[commands(about, ping)]
+
+struct General;
+
+
+
+#[command]
+async fn about(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "My name is Korg. I am a bot made  by Sir Matrix in rust for polling and other needs!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "pong!").await?;
+
+    Ok(())
+}
